c00646fc9c4667274bf92cc3ceabb96f1fb650dc
Kalyanisunil <kalyanisunil247@gmail.com>
2024-09-02 10:34:24 +0530

    added files

--- lib/auth.dart
+++ lib/auth.dart
@@ -23,17 +23,7 @@ Future<String> signUp(username, password, name) async {
   return "Error";
 }
 
-// void testuser() async {
-//   print("testuser");
-//   await FirebaseAuth.instance
-//       .createUserWithEmailAndPassword(
-//           email: 'shravan@gmail.com', password: 'Vazha@1234')
-//       .then((value) {
-//     return 'Success';
-//   }).catchError((onError) {
-//     print(onError.toString());
-//   });
-// }
+
 
 Future logout() async {
   await FirebaseAuth.instance.signOut();
